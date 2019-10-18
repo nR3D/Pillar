2a1db2285467a2dba73226eacc108b47cad1ca2a
nR3D <nR3D@pm.me>
2019-10-18 10:01:30 +0200

    z-depth color rendering

--- pil.py
+++ pil.py
@@ -5,9 +5,10 @@ from objs.create import *
 if __name__ == "__main__":
     o2 = cylinder(height=0.1, radius=3, inner_radius=1.7, segments=100)
     o2.rotate(0,0,10)
-    oi = import_obj('./objs/sphere.obj')
-    oi.scale(2)
-    coordinate_system = transform_matrix(rotation=(20,0,0), translation=(0,0,-7), scaling=(1,1,1))
+    oi = import_obj('./objs/pumpkin.obj')
+    oi.scale(1)
+    plane = cube(10,0.1,2)
+    coordinate_system = transform_matrix(rotation=(20,0,0), translation=(0,-2,-7), scaling=(1,1,1))
     print("Camera coordinate system:", coordinate_system, sep='\n')
-    render(coordinate_system, [o2,oi], WIREFRAME=True, DRAW_VERTICES=False)
+    render(coordinate_system, [oi,plane], WIREFRAME=False, DRAW_VERTICES=False, SOLID=True)
     
--- renderer.py
+++ renderer.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter
 import numpy as np
 
 def transform_matrix(**kwargs) -> np.matrix:
@@ -30,12 +30,32 @@ def transform_matrix(**kwargs) -> np.matrix:
         result_matrix[2,2] *= z
     return np.matrix(result_matrix)
 
+def blend_color(color1 : str, color2 : str, scale=0.5) -> str:
+    """
+        Blend two colors in a scale between 0 and 1
+        e.g color1 = '#000000', color2 = '#FFFFFF', scale = 0.5 (i.e. 50%)
+        scale=0 returns color1, scale=1 returns color2
+    """
+    if color1[0] != '#' or color2[0] != '#':
+        assert "Colors must be a string of hex values"
+    if scale > 1:
+        scale = 1
+    elif scale < 0:
+        scale = 0
+    
+    result = '#'
+    R = hex(round((int(color2[1:3], 16) - int(color1[1:3], 16))*scale + int(color1[1:3], 16)))[2:]
+    G = hex(round((int(color2[3:5], 16) - int(color1[3:5], 16))*scale + int(color1[3:5], 16)))[2:]
+    B = hex(round((int(color2[5:7], 16) - int(color1[5:7], 16))*scale + int(color1[5:7], 16)))[2:]
+    result += R if len(R) == 2 else '0'+R
+    result += G if len(G) == 2 else '0'+G
+    result += B if len(B) == 2 else '0'+B
+    return result
+
 def render_vertices(vertices : list, img : Image.Image, color="#00FF00"):
-    draw = ImageDraw.Draw(img)
-    draw.point(tuple((v[0],v[1]) for v in vertices), fill=color)  # draw vertices
+    ImageDraw.Draw(img).point(tuple((v[0],v[1]) for v in vertices), fill=color)  # draw vertices
 
 def render_wireframe(faces : list, img : Image.Image, color="#FF0000"): 
-    draw = ImageDraw.Draw(img)
     edges = list()
     for i in range(len(faces)):
         # connect near vertices to an edge
@@ -45,11 +65,10 @@ def render_wireframe(faces : list, img : Image.Image, color="#FF0000"):
             edges.append(faces[0])
         else:
             edges.append(faces[i+1])
-    draw.line(edges, fill=color)
+    ImageDraw.Draw(img).line(edges, fill=color)
 
 def render_solid_face(faces : list, img : Image.Image, color="#888888"): 
-    draw = ImageDraw.Draw(img)
-    draw.polygon(faces, fill=color)
+    ImageDraw.Draw(img).polygon(faces, fill=color)
 
 def render(camera : np.matrix, objects=[], **kwargs):
     """ 
@@ -61,14 +80,19 @@ def render(camera : np.matrix, objects=[], **kwargs):
     # canvas virtual size (pixel size is expressed by img.size)
     w = 1
     h = w * img.size[1]/img.size[0]
-
-    points_3d_to_2d = dict()  # keep track of drawn vertices by each object
+    
+    vertices = list()
+    z_depth = list()
+    faces = list()
     for obj in objects:
+        # copy faces adjusting the index value for each object
+        previous_len = len(vertices)
+        for face in obj.faces:
+            faces.append([f+previous_len for f in face])
         # for each object convert vertices to camera CoordinateSystem and draw them
-        vertices = list()
         for vertex in obj.iter_vertices(camera):
             # convert 3d space to canvas space
-            if vertex[2] > 0:
+            if vertex[2] > 0:  # debug print
                 print("Not visible, vertex:", vertex[:3])
             if not vertex[2]:  # vertex[2] == 0 would lead to division by zero in the next expression
                 x = img.size[0] * vertex[0]
@@ -77,34 +101,38 @@ def render(camera : np.matrix, objects=[], **kwargs):
                 x = img.size[0] * ((vertex[0] / -vertex[2]) + w/2)/w
                 y = img.size[1] * (1-((vertex[1] / -vertex[2]) + h/2)/h)
             vertices.append((x,y))
-        depth_faces = list()
-        for face in obj.faces:
-            if not len(depth_faces):
-                depth_faces.append(face)
-            else:
-                for i in range(len(depth_faces)):  # raw z-depth implementation (doesn't work for crossing faces)
-                    if sum([obj.vertices[f-obj.INDEX_OFFSET][2] for f in face])/len(face) < sum(
-                            obj.vertices[f-obj.INDEX_OFFSET][2] for f in depth_faces[i])/len(depth_faces[i]):
-                        depth_faces.insert(i, face)
-                        break
-                    elif i == len(depth_faces) - 1:
-                        depth_faces.append(face)
-        for face in depth_faces:
-            # draw faces (or wireframe)
-            draw_face = list()
-            visible = False
-            for f in face:
-                draw_face.append(vertices[f-obj.INDEX_OFFSET])
-                x, y = draw_face[-1]
-                if (x >= 0 and x <= img.size[0]) or (y >= 0 and y <= img.size[1]):
-                    visible = True
-            if visible:
-                if kwargs['WIREFRAME'] if 'WIREFRAME' in kwargs else obj.WIREFRAME:
-                    render_wireframe(draw_face, img)
-                else:
-                    render_solid_face(draw_face, img)
-        if kwargs['DRAW_VERTICES'] if 'DRAW_VERTICES' in kwargs else obj.DRAW_VERTICES:
-            render_vertices(vertices, img)
-        points_3d_to_2d[obj] = vertices
+            z_depth.append(vertex[2])
+    
+    depth_faces = list()
+    for face in faces:
+        if not len(depth_faces):
+            depth_faces.append(face)
+        else:
+            for i in range(len(depth_faces)):  # raw z-depth implementation (doesn't work for crossing faces)
+                if sum([z_depth[f-kwargs.get('INDEX_OFFSET', 1)] for f in face])/len(face) < sum(
+                        z_depth[f-kwargs.get('INDEX_OFFSET', 1)] for f in depth_faces[i])/len(depth_faces[i]):
+                    depth_faces.insert(i, face)
+                    break
+                elif i == len(depth_faces) - 1:
+                    depth_faces.append(face)
+    
+    for face in depth_faces:
+        # draw faces (or wireframe)
+        draw_face = list()
+        visible = False
+        for f in face:
+            draw_face.append(vertices[f-kwargs.get('INDEX_OFFSET', 1)])
+            x, y = draw_face[-1]
+            if (x >= 0 and x <= img.size[0]) or (y >= 0 and y <= img.size[1]):
+                visible = True
+        if visible:
+            if kwargs.get('SOLID', True):
+                z_face = [z_depth[f-kwargs.get('INDEX_OFFSET', 1)] for f in face]
+                color = blend_color('#ff9000', '#000000', float(sum(z_face)/(len(face)*min(z_depth))))
+                render_solid_face(draw_face, img, color)
+            if kwargs.get('WIREFRAME'):
+                render_wireframe(draw_face, img)
+            if kwargs.get('DRAW_VERTICES'):
+                render_vertices([vertices[f-kwargs.get('INDEX_OFFSET', 1)],], img)
     img.show()
 
